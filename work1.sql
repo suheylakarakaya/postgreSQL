e70ef9c6f93417f4d4fac2825542b0a839665c37
suheylakarakaya <45814285+suheylakarakaya@users.noreply.github.com>
2022-01-30 20:28:05 +0300

    Update work1.sql
    edited

--- work1.sql
+++ work1.sql
@@ -1,25 +1,29 @@
---SELECT title, description FROM film;
+/*1*/ 
+SELECT title, description FROM film;
 
-/*SELECT * FROM film
-WHERE length > 60 AND length < 70; */
+/*2*/
+SELECT * FROM film
+WHERE length > 60 AND length < 70; 
 
-/*SELECT * FROM film
-WHERE (replacement_cost = 12.99 OR replacement_cost =28.99) AND rental_rate = 0.99; */
+/*3*/
+SELECT * FROM film
+WHERE (replacement_cost = 12.99 OR replacement_cost =28.99) AND rental_rate = 0.99; 
 
-/* SELECT first_name, last_name FROM customer
-WHERE first_name = 'Mary'; */ 
+/*4*/ 
+SELECT first_name, last_name FROM customer
+WHERE first_name = 'Mary'; 
 
-/*SELECT * FROM film
-WHERE NOT length > 50 AND rental_rate != 2.99 OR rental_rate != 4.99; */
+/*5*/ 
+SELECT * FROM film
+WHERE NOT length > 50 AND rental_rate != 2.99 OR rental_rate != 4.99; 
 
 
 /*Merhabalar,
 
-Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden alınan veriler ile gerçekleştirdim.
 
-film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
-film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
-film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
-customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
-film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
-Kolay Gelsin. */
+1- film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
+2- film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
+3- film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
+4- customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
+5- film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız. */
