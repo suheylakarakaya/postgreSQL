14a4ab8ca8c353ff35151ce93f2163f6cb6f7bc1
suheylakarakaya <45814285+suheylakarakaya@users.noreply.github.com>
2022-02-11 16:37:50 +0300

    Update work4.sql

--- work4.sql
+++ work4.sql
@@ -11,3 +11,12 @@ WHERE country LIKE '_____';
 --5
 SELECT COUNT(city) FROM city
 WHERE city ILIKE '%R';
+
+
+-- Aşağıdaki sorgu senaryoları dvdrental örnek veri tabanı üzerinden gerçekleştirilmiştir.
+
+1- film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+2- film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+3- film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+4- country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+5- city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
